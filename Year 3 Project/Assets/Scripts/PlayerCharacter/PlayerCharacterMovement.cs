e72962ff583e091b75f5e2c3e845af852e33879d
Oliver Richardson <or83@kent.ac.uk>
2023-08-15 00:28:01 +0100

    Initial Set up of project
    added a starting scene
    added a testing scene
    added a player character
    added a player character movement script
    linked the camera to player movement
    made the player character a prefab

---
+++ Year 3 Project/Assets/Scripts/PlayerCharacter/PlayerCharacterMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerCharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate is called 50 times per second
+    private void FixedUpdate()
+    {
+        // Create empty Vector2
+        Vector2 direction = Vector2.zero;
+        // Evaluate horizontal input
+        if(Input.GetKey(KeyCode.A))
+        {
+            direction.x = -1;
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            direction.x = 1;
+        }
+        // Evaluate verticle input
+        if(Input.GetKey(KeyCode.S))
+        {
+            direction.y = -1;
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            direction.y = 1;
+        }
+        // Normalize Vector2 so that it isn't faster to travel diagonally
+        direction.Normalize();
+        // Add velocity to the character
+        GetComponent<Rigidbody2D>().velocity = speed * direction;
+    }
+}
