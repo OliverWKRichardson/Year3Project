52bac1908d4ce20360e1be1c486b46ce1609f0bb
Oliver Richardson <or83@kent.ac.uk>
2023-09-25 20:59:29 +0100

    fix: recommit of combat screen
    Reverting the force commit remove the commit for the combat screen from the branch so I have redownloaded that version and committed it here. As a result it is likely incompatible and everyone may have to redownload the project from this commit to continue.

---
+++ Year 3 Project/Assets/Scripts/CombatScreen/CombatScreen.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Initiate Combat Here
+    // Use Sprite Spawn Empties to create sprites of combatants
+    // Get the stats of the combatants
+    // Decide starting turn
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Use to change UI on changes in combat
+    // Change turn indicator when turn changes
+    void Update()
+    {
+        
+    }
+}
