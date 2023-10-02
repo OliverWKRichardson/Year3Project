7bfd64da1f609d4167ebb2e7eac7ab755eddb835
Emily <emstarkie@gmail.com>
2023-10-03 00:01:15 +0100

    !feat: added basic implemetation of quiz UI

---
+++ Year 3 Project/Assets/Scripts/QuizSystem/QA.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QA : MonoBehaviour
+{
+    public GameObject MenuCenter;
+
+    public GameObject question;
+
+    // change to arrays
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+
+    public GameObject AnswerText1;
+    public GameObject AnswerText2;
+    public GameObject AnswerText3;
+
+
+
+    void Start()
+    {
+       question = GameObject.Find("QuestionText");
+       question.GetComponent<UnityEngine.UI.Text>().text = "What is the capital of the United States?";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Year 3 Project/Assets/Scripts/QuizSystem/QuizSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizSpawner : MonoBehaviour
+{
+
+    void onCollision(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            spawnQuiz();
+        }
+    }
+
+    void spawnQuiz()
+    {
+        // spawn quiz
+        GameObject quiz = Instantiate(Resources.Load("Prefabs/UI/QuizSystem/QuizSpawner")) as GameObject;
+        // set quiz position
+        quiz.transform.position = new Vector3(0, 0, 0);
+        // set quiz parent
+        quiz.transform.parent = GameObject.Find("Canvas").transform;
+        // set quiz name ??
+        quiz.name = "Quiz";
+ ??    } ??
+}
