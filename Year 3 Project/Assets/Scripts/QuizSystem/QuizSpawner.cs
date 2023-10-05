ab4147a77c58341bd0b931b6f57f22f96f157586
Emily <emstarkie@gmail.com>
2023-10-05 22:23:15 +0100

    chore: refactored quiz collision

--- Year 3 Project/Assets/Scripts/QuizSystem/QuizSpawner.cs
+++ Year 3 Project/Assets/Scripts/QuizSystem/QuizSpawner.cs
@@ -1,18 +1,46 @@
-    using System.Collections;
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuizSpawner : MonoBehaviour
 {
-    void spawnQuiz()
+    public bool quizTriggered = false;
+    GameObject quizScreen;
+    public GameObject quizScreenPrefab;
+
+    public string quizScreenPrefabPath = "QA"; 
+
+    Vector3 quizScreenPosition = new Vector3(0, 0, 0);
+
+    private PlayerCharacterMovement playerMovement;
+
+
+    void Update()
     {
-        // spawn quiz
-        GameObject quiz = Instantiate(Resources.Load("Prefabs/UI/QuizSystem/QuizSpawner")) as GameObject;
-        // set quiz position
-        quiz.transform.position = new Vector3(0, 0, 0);
-        // set quiz parent
-        quiz.transform.parent = GameObject.Find("Canvas").transform;
-        // set quiz name
-        quiz.name = "Quiz";
+
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered range");
+            quizScreen = Resources.Load<GameObject>(quizScreenPrefabPath);
+
+            if (!quizTriggered)
+            {
+                playerMovement = other.GetComponent<PlayerCharacterMovement>();
+                playerMovement.DisablePlayerMovement();
+               // Instantiate(quizScreenPrefab, quizScreenPosition, Quaternion.identity);
+                Instantiate(quizScreen, quizScreenPosition, Quaternion.identity);
+                quizTriggered = true;
+                GetComponent<Collider2D>().enabled = false;
+            }
+        }
+    }
+
+
+
 }
