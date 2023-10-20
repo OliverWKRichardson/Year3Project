da74f67621ab770061ef41f09f1378d0d47a9e27
aik2047 <ak2041@kent.ac.uk>
2023-10-20 13:15:15 +0100

    feat added range to enemy
    Testing range

--- Year 3 Project/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Year 3 Project/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -35,7 +35,9 @@ namespace Pathfinding {
 
 		/// <summary>Updates the AI's destination every frame</summary>
 		void Update () {
-			if (target != null && ai != null) ai.destination = target.position;
+			if ((target != null && ai != null) && (Vector2.Distance(transform.position, target.position) > 10)){
+				ai.destination = target.position;
+			}
 		}
 	}
 }
