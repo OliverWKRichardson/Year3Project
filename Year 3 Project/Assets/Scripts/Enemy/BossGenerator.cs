2b730cec0c548e842f1ba1d03b0869b6de43fe48
aik2047 <ak2041@kent.ac.uk>
2023-11-01 02:27:11 +0000

    feat: tutorial boss added

---
+++ Year 3 Project/Assets/Scripts/Enemy/BossGenerator.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static CombatScreen.TurnType;
+using System;
+using UnityEditor.UI;
+
+public class BossGenerator : MonoBehaviour
+{
+    // Enemy class
+    public GameObject boss;
+
+    //enemy
+    GameObject hostile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createBoss();
+    }
+
+    void createBoss()
+    {
+        Transform pos = gameObject.transform;
+        hostile = Instantiate(boss, pos); // Add Vector2(x, y) for the position of the newly generated boss
+        hostile.GetComponent<EnemyStats>().setSPD(UnityEngine.Random.Range(11, 15));
+        hostile.GetComponent<EnemyStats>().setMaxHP(UnityEngine.Random.Range(800, 1000));
+        hostile.GetComponent<EnemyStats>().setHP(hostile.GetComponent<EnemyStats>().getMaxHP());
+        hostile.GetComponent<EnemyStats>().setMaxMP(UnityEngine.Random.Range(200, 250));
+        hostile.GetComponent<EnemyStats>().setMP(hostile.GetComponent<EnemyStats>().getMaxMP());
+        hostile.GetComponent<EnemyStats>().setATK(UnityEngine.Random.Range(120, 150));
+        hostile.GetComponent<EnemySkills>().SetSkills(DoTC, DoTCCost, DoTA, DoTACost, DoTI, DoTICost);
+    }
+
+
+    // Template Skills
+    public void DoTA(GameObject target)
+    {
+        Debug.Log("Enemy Used DoTA Attack On "+ target.name);
+        Condition DDoS = new Condition("DoTA", 5, CombatScreen.TurnType.playerTurn, 10.0f);
+        target.GetComponent<PlayerStats>().AddCondition(DDoS);
+        hostile.GetComponent<EnemyStats>().spendMP(50);
+    }
+    private int DoTACost = 50;
+    public void DoTI(GameObject target)
+    {
+        Debug.Log("Enemy Used DoTI Attack On "+ target.name);
+        Condition FileDeletionWorm = new Condition("DoTI", 5, CombatScreen.TurnType.playerTurn, 10.0f);
+        target.GetComponent<PlayerStats>().AddCondition(FileDeletionWorm);
+        hostile.GetComponent<EnemyStats>().spendMP(50);
+    }
+    private int DoTICost = 50;
+    public void DoTC(GameObject target)
+    {
+        Debug.Log("Enemy Used DoTA Attack On "+ target.name);
+        Condition DownloadingPersonalData = new Condition("DoTC", 5, CombatScreen.TurnType.playerTurn, 10.0f);
+        target.GetComponent<PlayerStats>().AddCondition(DownloadingPersonalData);
+        hostile.GetComponent<EnemyStats>().spendMP(50);
+    }
+    private int DoTCCost = 50;
+
+    public void HeavyAttack(GameObject target)
+    {
+        Debug.Log("Enemy Used Heavy Attack On "+ target.name);
+        hostile.GetComponent<EnemyStats>().spendMP(100);
+        float amount = hostile.GetComponent<EnemyStats>().getATK();
+        float MULT = 2;
+        target.GetComponent<PlayerStats>().DamageC(amount*MULT);
+        target.GetComponent<PlayerStats>().DamageI(amount*MULT);
+        target.GetComponent<PlayerStats>().DamageA(amount*MULT);
+    }
+    private int HeavyAttackCost = 100;
+    public void LightAttack(GameObject target)
+    {
+        Debug.Log("Enemy Used Light Attack On "+ target.name);
+        float amount = hostile.GetComponent<EnemyStats>().getATK();
+        target.GetComponent<PlayerStats>().DamageC(amount);
+        target.GetComponent<PlayerStats>().DamageI(amount);
+        target.GetComponent<PlayerStats>().DamageA(amount);
+    }
+    private int LightAttackCost = 0;
+}
+
