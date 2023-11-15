2193800318fb97f8b7f4b6ac49400a33616fc019
aik2047 <ak2041@kent.ac.uk>
2023-11-15 12:48:03 +0000

    feat: enemies now consistent in combat screen

--- Year 3 Project/Assets/Scripts/Enemy/SpriteRandomiser.cs
+++ Year 3 Project/Assets/Scripts/Enemy/SpriteRandomiser.cs
@@ -6,11 +6,13 @@ public class SpriteRandomiser : MonoBehaviour
 {
     private int rand;
     public Sprite[] SpriteOptions;
+    public GameObject[] GOSpriteOptions;
     // Start is called before the first frame update
     void Start()
     {
         rand = Random.Range(0,SpriteOptions.Length);
         GetComponent<SpriteRenderer>().sprite = SpriteOptions[rand];
+        GetComponent<CombatSprite>().sprite = GOSpriteOptions[rand];
     }
 
     // Update is called once per frame
