fbf25671c55efc7b0979f0d30ce873d14eaf0311
gnf3Kent <gnf3@Kent.ac.uk>
2024-01-18 14:34:21 +0000

    Feat: Shop Complete, Money System linked to shop, Abilities complete
    Finished Shop, Abilities and linked my money system to the shop.

--- Year 3 Project/Assets/Scripts/Environment/ShopInteract.cs
+++ Year 3 Project/Assets/Scripts/Environment/ShopInteract.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ShopInteract : MonoBehaviour
 {
@@ -39,8 +40,8 @@ public class ShopInteract : MonoBehaviour
             }
         }
 
-       
-        
+
+
 
 
     }
@@ -60,10 +61,10 @@ public class ShopInteract : MonoBehaviour
         {
             entered = false;
             interact.SetActive(false);
-            
+
             closeShop();
-  
-            
+
+
         }
 
         //Destroy SHOP GUI here.
@@ -105,29 +106,136 @@ public class ShopInteract : MonoBehaviour
         Exitbtn.SetActive(true);
         Helpbtn.SetActive(true);
     }
-//Need to take money on purchases in future game
-    public void buyAC() {
+    //Need to take money on purchases in future game
+    public void buyAC()
+    {
         //Heal C to full 
+        Debug.Log("Buying!");
+        int money = gameObject.GetComponent<PlayerStats>().GetMoney();
+        int price = GameObject.FindWithTag("ShopItems").gameObject.transform.Find("AC").gameObject.GetComponent<PriceScript>().getpriceVal();
+        if (money >= price)
+        {
+            Debug.Log("AC1");
+            stats.setC(stats.getMaxC());
+            sendMsg("Access Control designated, running TECHNICAL control to REACT to bad actors in the network. Changes access levels of roles/members in the organization's network and increases the confidentiality of your system. ");
+            //
+        }
+        else
+        {
+            sendMsg("You don't have enough NETCOIN.");
 
-        stats.setC(stats.getMaxC());
-    
+        }
+
     }
 
-    public void buyRS() {
+    public void buyRS()
+    {
+
         //Heal I to full
-        stats.setI(stats.getMaxI());
+        int money = gameObject.GetComponent<PlayerStats>().GetMoney();
+        int price = GameObject.FindWithTag("ShopItems").gameObject.transform.Find("RS").gameObject.GetComponent<PriceScript>().getpriceVal();
+        if (money >= price)
+        {
+            stats.setI(stats.getMaxI());
+            sendMsg("Recovery system booted, running TECHNICAL control to REACT to active data loss from your system, restores lost data using back-ups and other protocols. Increases the integrity of your system. .");
+        }
+        else
+        {
+            sendMsg("You don't have enough NETCOIN.");
+
+        }
     }
 
     public void buyAVS()
     {   //Heal A to full
-        stats.setA(stats.getMaxA());
+        int money = gameObject.GetComponent<PlayerStats>().GetMoney();
+        int price = GameObject.FindWithTag("ShopItems").gameObject.transform.Find("AVS").gameObject.GetComponent<PriceScript>().getpriceVal();
+        if (money >= price)
+        {
+            stats.setA(stats.getMaxA());
+            sendMsg("Antivirus system enabled, running TECHNICAL control to REACT to active viruses & malware from your system and remove them, increases the availability of your system.");
+        }
+        else
+        {
+            sendMsg("You don't have enough NETCOIN.");
+
+        }
+
     }
-   
+
     public void buyFW()
     {
-        gameObject.transform.Find("Firewall").gameObject.SetActive(true);
+        int money = gameObject.GetComponent<PlayerStats>().GetMoney();
+        int price = GameObject.FindWithTag("ShopItems").gameObject.transform.Find("FW").gameObject.GetComponent<PriceScript>().getpriceVal();
+        ;
+        if (money >= price)
+        {
+            gameObject.transform.Find("Firewall").gameObject.SetActive(true);
+            sendMsg("Firewall initialized. Running TECHNICAL control to PREVENT threats from enaging your system.");
+        }
+        else
+        {
+            sendMsg("You don't have enough NETCOIN.");
+        }
 
-        
     }
+    public void buyIRP()
+    {
+        int money = gameObject.GetComponent<PlayerStats>().GetMoney();
+        int price = GameObject.FindWithTag("ShopItems").gameObject.transform.Find("IRP").gameObject.GetComponent<PriceScript>().getpriceVal();
+        if (money >= price)
+        {
+            GameObject[] gb = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (var item in gb)
+            {
+                EnemyStats es = item.gameObject.GetComponent<EnemyStats>();
+                int rng = Random.Range(0, 2);
+
+                //Weakenn The Enemies
+                es.setATK(0.75f * es.getATK());
+
+            }
+            sendMsg("Incident Response Plan initiated. Running ADMINISTRATIVE control to REACT to and weaken existing threats in the system.");
+        }
+        else
+        {
+            sendMsg("You don't have enough NETCOIN.");
+
+        }
+    }
+
+    public void buyIDS()
+    {
+        int money = gameObject.GetComponent<PlayerStats>().GetMoney();
+        int price = GameObject.FindWithTag("ShopItems").gameObject.transform.Find("IDS").gameObject.GetComponent<PriceScript>().getpriceVal();
+        if (money >= price)
+        {
+            
+            //Intrustion Detection System
+            GameObject[] gb = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (var item in gb)
+            {
+                item.gameObject.transform.Find("EnemyMMapSprite").gameObject.SetActive(true);
+            }
+            sendMsg("Intrusion Detection System activated. Running TECHNICAL control scans to DETECT threats, they will appear on your radar.");
+        }
+        else
+        {
+            sendMsg("You don't have enough NETCOIN.");
+
+        }
+
+    }
+
+    private void sendMsg(String msg)
+    {
+        DialogScript ds = GameObject.FindWithTag("DialogueSystem").gameObject.GetComponent<DialogScript>();
+        string[] dialog = new string[1];
+        dialog[0] = msg;
+        ds.addDialog(dialog);
+    }
+
+    
+
 
 }
--- Year 3 Project/Assets/Scripts/dialogtest.cs
+++ Year 3 Project/Assets/Scripts/dialogtest.cs
@@ -7,11 +7,13 @@ public class dialogtest : MonoBehaviour
 
     public GameObject dialog;
     private DialogScript ds;
+    public Sprite img;
     
     // Start is called before the first frame update
     void Start()
     {
         ds = dialog.GetComponent<DialogScript>();
+        ds.SetImg(img); 
         string[] msg = new string[2];
         msg[0] = "Network layer 1 loaded.";
         msg[1] = "This is the first and least challenging layer of the network, for each threat you neutralize you will earn NETCOINS, Spend NETCOINS at the World Wide Web to upgrade & maintenance your AI model.";
